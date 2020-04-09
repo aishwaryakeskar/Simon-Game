5ff87c18ceede7868a029ed799ce6775b2794b6c
Aishwarya Keskar <aishwaryakeskar98@gmail.com>
2020-04-09 21:29:05 +0530

    Added sound and animation

--- game.js
+++ game.js
@@ -2,11 +2,20 @@ var buttonColors = ["red","blue", "green", "yellow"]
 var gamePattern = []
 
 // generate the next color sequence
-function nextSequence() {
+function nextSequence() { 
     var randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber; 
+    var randomChosenColour = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+
+    // Select button with same id as the randomChosenColour
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+
+    console.log(randomChosenColour);
+
 }
 
-var randomChosenColour = buttonColors[nextSequence()];
-
-gamePattern.push(randomChosenColour);
+$("body").click(function(){nextSequence()});
