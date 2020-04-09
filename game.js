d5e02f7979206e6745728c74e9800ae29b0b2547
Aishwarya Keskar <aishwaryakeskar98@gmail.com>
2020-04-09 22:30:26 +0530

    Added sound and animation to user click

--- game.js
+++ game.js
@@ -1,21 +1,44 @@
 var buttonColors = ["red","blue", "green", "yellow"]
 var gamePattern = []
+var userClickedPattern = []
 
 // generate the next color sequence
 function nextSequence() { 
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColors[randomNumber];
-
     gamePattern.push(randomChosenColour);
+    
+    playSound(randomChosenColour);
 
     // Select button with same id as the randomChosenColour
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
-
-    console.log(randomChosenColour);
+    
+    console.log("randomColour:" + randomChosenColour);
 
 }
 
-$("body").click(function(){nextSequence()});
+// User button clicks
+$(".btn").click(function() {
+    nextSequence();
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    console.log("userPattern:" + userClickedPattern);
+});
+
+// Plays sound according to the colour
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+
+// Animate button when clicked
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
