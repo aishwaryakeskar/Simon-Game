e8388dde0fe6137bac66257ec1088c6de70cb533
Aishwarya Keskar <aishwaryakeskar98@gmail.com>
2020-04-10 18:18:29 +0530

    Checked user game pattern

--- game.js
+++ game.js
@@ -1,44 +1,76 @@
-var buttonColors = ["red","blue", "green", "yellow"]
-var gamePattern = []
-var userClickedPattern = []
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
 
-// generate the next color sequence
-function nextSequence() { 
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColour);
-    
-    playSound(randomChosenColour);
-
-    // Select button with same id as the randomChosenColour
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    
-    console.log("randomColour:" + randomChosenColour);
-
-}
+// Start game on first key press
+var gameStarted = false;
+$(document).keydown(function () {
+  if (gameStarted == false) {
+    $("h1").text("Level " + level);
+    nextSequence();
+  }
+  gameStarted = true;
+});
 
 // User button clicks
-$(".btn").click(function() {
-    nextSequence();
-    var userChosenColor = $(this).attr("id");
-    userClickedPattern.push(userChosenColor);
-    playSound(userChosenColor);
-    animatePress(userChosenColor);
-    console.log("userPattern:" + userClickedPattern);
+$(".btn").click(function () {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  checkAnswer(userChosenColor);
+
+  console.log("last color index :" + buttonColors.indexOf(userChosenColor));
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  console.log("userPattern:" + userClickedPattern);
 });
+
+var i = 0;
+
+function checkAnswer(currentLevel) {
+  if (currentLevel == gamePattern[i]) {
+    console.log("Success");
+    i++;
+    if (userClickedPattern.length == gamePattern.length) {
+      setTimeout(nextSequence, 1000);
+      userClickedPattern = [];
+      i = 0;
+    }
+  } else {
+    console.log("Wrong");
+  }
+}
+
+// generate the next color sequence
+function nextSequence() {
+  level++;
+  $("h1").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  playSound(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  console.log("gamePattern:" + gamePattern);
+}
 
 // Plays sound according to the colour
 function playSound(name) {
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
-
 
 // Animate button when clicked
 function animatePress(currentColour) {
-    $("#" + currentColour).addClass("pressed");
-    setTimeout(function() {
-        $("#" + currentColour).removeClass("pressed");
-    }, 100);
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
